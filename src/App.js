f6ac1f7ae2ed60477d700d2e34aa11777e876536
Soubhagya Samal <91544002+Soubhagya118@users.noreply.github.com>
2022-05-05 08:49:30 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
 import {Checkout} from './components/CheckOut';
+import Navbar from './components/Navbar';
+import {BrowserRouter} from "react-router-dom";
+import Footer  from './components/Footer';
+import LeftBar from './components/LeftBar';
 
 
 function App() {
   return (
     <div className="App">
+    <BrowserRouter>
       <Checkout/>
+    <Navbar/>
+    <LeftBar/>
+    <Footer/>
+    </BrowserRouter>
     </div>
   );
 }
